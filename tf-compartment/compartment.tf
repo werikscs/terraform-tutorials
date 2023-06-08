538f4e94a2d1d22941037c7e9d9953271dd606db
Weriks Santos <werikscs@gmail.com>
2023-06-07 22:00:00 -0300

    remove pastas não utilizadas

--- tf-compartment/compartment.tf
+++
@@ -1,5 +0,0 @@
-resource "oci_identity_compartment" "tf-compartment" {
-  compartment_id = var.compartment_id
-  description    = "Compartment for Terraform resources."
-  name           = "CompartmentForTutorial"
-}
