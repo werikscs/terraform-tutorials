99815389c6816ea5f799a4d85245ffdb3bc5cfd7
Weriks Santos <werikscs@gmail.com>
2023-06-15 13:27:06 -0300

    adiciona integração com Ansible

--- tf-compute/compute.tf
+++ tf-compute/compute.tf
@@ -1,24 +1,14 @@
 resource "oci_core_instance" "ubuntu_instance" {
-  # Required
   availability_domain = data.oci_identity_availability_domains.ads.availability_domains[1].name
   compartment_id      = var.compartment_id
 
-  # MICRO
   shape = "VM.Standard.E2.1.Micro"
-
-  # # FLEX
-  # shape = "VM.Standard.A1.Flex"
-  # shape_config {
-  #   memory_in_gbs = "6"
-  #   ocpus         = "2"
-  # }
 
   source_details {
     source_id   = var.source_ocid
     source_type = "image"
   }
 
-  # Optional
   display_name = "ubuntu_tutorial"
   create_vnic_details {
     assign_public_ip = true
@@ -32,28 +22,32 @@ resource "oci_core_instance" "ubuntu_instance" {
   preserve_boot_volume = false
 
   provisioner "remote-exec" {
-    inline = [
-      "sudo apt-get update",
-      "sudo apt-get install -y docker.io",
-      "sudo systemctl start docker",
-      "sudo systemctl enable docker",
-      "sudo usermod -aG docker ubuntu",
-      "echo ${var.docker_password} | sudo docker login --username ${var.docker_username} --password-stdin",
-      "sudo docker run -d -p ${var.instance_port}:${var.container_port} --name api ${var.docker_username}/${var.image_name}:${var.image_tag_name}",
-    ]
     connection {
       host        = self.public_ip
       type        = "ssh"
       user        = "ubuntu"
       private_key = file(var.ssh_private_key_path)
     }
+
+    inline = ["[CONNECTED!]"]
+  }
+
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${self.public_ip},' --user ubuntu --private-key ${var.ssh_private_key_path} ../ansible/playbook.yml"
+
+    environment = {
+      DOCKER_USERNAME       = var.docker_username
+      DOCKER_PASSWORD       = var.docker_password
+      DOCKER_IMAGE_NAME     = var.image_name
+      DOCKER_IMAGE_TAG_NAME = var.image_tag_name
+      INSTANCE_PORT         = var.instance_port
+      CONTAINER_PORT        = var.container_port
+    }
   }
 }
 
-# Atualiza o registro DNS no No-IP com o IP da instância OCI
 resource "null_resource" "update_noip_dns" {
   depends_on = [oci_core_instance.ubuntu_instance]
-
   provisioner "local-exec" {
     command = <<-EOT
       curl -k -u "${var.noip_username}:${var.noip_password}" "http://dynupdate.no-ip.com/nic/update?hostname=${var.noip_hostname}&myip=${oci_core_instance.ubuntu_instance.public_ip}"
--- tf-compute/outputs.tf
+++ tf-compute/outputs.tf
@@ -8,34 +8,34 @@ output "public-ip-for-compute-instance" {
   value = oci_core_instance.ubuntu_instance.public_ip
 }
 
-output "instance-name" {
-  value = oci_core_instance.ubuntu_instance.display_name
-}
+# output "instance-name" {
+#   value = oci_core_instance.ubuntu_instance.display_name
+# }
 
-output "instance-OCID" {
-  value = oci_core_instance.ubuntu_instance.id
-}
+# output "instance-OCID" {
+#   value = oci_core_instance.ubuntu_instance.id
+# }
 
-output "instance-region" {
-  value = oci_core_instance.ubuntu_instance.region
-}
+# output "instance-region" {
+#   value = oci_core_instance.ubuntu_instance.region
+# }
 
-output "instance-shape" {
-  value = oci_core_instance.ubuntu_instance.shape
-}
+# output "instance-shape" {
+#   value = oci_core_instance.ubuntu_instance.shape
+# }
 
-output "instance-state" {
-  value = oci_core_instance.ubuntu_instance.state
-}
+# output "instance-state" {
+#   value = oci_core_instance.ubuntu_instance.state
+# }
 
-output "instance-OCPUs" {
-  value = oci_core_instance.ubuntu_instance.shape_config[0].ocpus
-}
+# output "instance-OCPUs" {
+#   value = oci_core_instance.ubuntu_instance.shape_config[0].ocpus
+# }
 
-output "instance-memory-in-GBs" {
-  value = oci_core_instance.ubuntu_instance.shape_config[0].memory_in_gbs
-}
+# output "instance-memory-in-GBs" {
+#   value = oci_core_instance.ubuntu_instance.shape_config[0].memory_in_gbs
+# }
 
-output "time-created" {
-  value = oci_core_instance.ubuntu_instance.time_created
-}
+# output "time-created" {
+#   value = oci_core_instance.ubuntu_instance.time_created
+# }
